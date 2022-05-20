4873fe6679dc776c039d097f49b2df909e5fed88
Gabriel <ilie.cgabriel@gmail.com>
2022-05-20 20:53:06 +0300

    Removed not needed style

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.green,
         useMaterial3: true,
       ),
       home: const ExploreScreen(),
