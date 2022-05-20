dac5bdc7a52fc15153627da14ae17c1277d5e693
Gabriel <ilie.cgabriel@gmail.com>
2022-05-20 18:59:10 +0300

    Fixed import statements

--- lib/screens/explore/explore_categories.dart
+++ lib/screens/explore/explore_categories.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:pokedex/screens/categories/pokedex/pokedex_screen.dart';
 import 'package:pokedex/screens/explore/category_card.dart';
 
